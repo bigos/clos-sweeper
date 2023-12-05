04075fa62ddf8b4bd192268e26d45ab734597f35
Jacek Podkanski <ruby.object@googlemail.com>
2023-12-05 00:31:16 +0000

    moved to other files

---
+++ events.lisp
@@ -0,0 +1,117 @@
+(in-package #:clos-sweeper)
+
+;;; ================================= All GUI Events ===========================
+
+;;; these functions are for REPL convenience to play with handling the events
+(defun de-menu-simple (action-name)           (process-event :menu-simple action-name))
+(defun de-menu-bool   (action-name bool01)    (process-event :menu-bool   action-name bool01))
+(defun de-menu-radio  (action-name radiostr)  (process-event :menu-radio  action-name radiostr))
+(defun de-motion      (x y)                   (process-event :motion  x y))
+(defun de-enter       (x y)                   (process-event :enter   x y))
+(defun de-leave       ()                      (process-event :leave))
+(defun de-pressed     (button x y)            (process-event :pressed   button x y))
+(defun de-released    (button x y)            (process-event :released  button x y))
+(defun de-scroll      (dx dy)                 (process-event :scroll    dx dy))
+(defun de-key-pressed (letter name code mods) (process-event :key-pressed  letter name code mods))
+(defun de-resize      (width height)          (process-event :resize width height))
+(defun de-timeout     ()                      (process-event :timeout))
+
+;;; ======================= update and view ====================================
+
+(let ((motion-count 0))
+  (defun process-event (event &rest args)
+    (if (eq event :motion)
+        (incf motion-count)
+        (setf motion-count 0))
+
+    (unless (member event  '(:motion :timeout))
+      (format t "~&processing event ~S ~S ~S~%" event (first args) (rest args)))
+    ;; like in Elm runtime calling the update
+    (ecase event
+      (:menu-simple (setf (selection *model*) (format nil "~S" args)))
+      (:menu-bool   (setf (selection *model*) (format nil "~S" args)))
+      (:menu-radio  (progn
+                      (setf (selection *model*) (format nil "~S" args))
+                      (when (and (equalp (first args) "new-game-size")
+                                 (equalp (rest args) '("SMALL")))
+                        (init-model (width *model*) (height *model*))
+                        (process-event :resize (width *model*) (height *model*)))))
+      (:motion (set-mouse *model* args))
+      (:enter  (set-mouse *model* args))
+      (:leave  (set-mouse *model* args))
+      (:pressed (ecase (first args)
+                  (1 (progn
+                       (setf (button-1 *model*) T)
+                       (process-pressed-1)))
+                  (2 (setf (button-2 *model*) T))
+                  (3 (progn (setf (button-3 *model*) T)
+                            (process-pressed-3)))))
+      (:released (ecase (first args)
+                   (1 (setf (button-1 *model*) nil))
+                   (2 (setf (button-2 *model*) nil))
+                   (3 (setf (button-3 *model*) nil))))
+      (:scroll )
+      (:key-pressed )
+      (:resize (progn
+                 (setf (width  *model*) (first  args)
+                       (height *model*) (second args))
+                 (resize-grid (grid *model*))))
+      (:timeout)
+      )
+
+    ;; inspired by Gtk4, think of separate from update layout phase
+    ;; https://docs.gtk.org/gtk4/drawing-model.html
+    ;; geometry changes calculated
+
+
+    ;; like in Elm runtime calling the view
+    (if *canvas*
+        (gtk4:widget-queue-draw *canvas*)
+        (simulate-draw-func (if (width *model*)  (width *model*)  800)
+                            (if (height *model*) (height *model*) 600)))))
+
+(defun mine-stats ()
+  (let ((covered-mines (length (loop for c in (children (grid *model*))
+                                     when (member (state c) '(:covered-mine))
+                                       collect c))))
+    (setf (selection *model*)
+          (cond
+            ((not (zerop (length (loop for c in (children (grid *model*))
+                                       when (member (state c) '(:exploded-mine))
+                                         collect c)) ))
+             (format nil "You lost!!!"))
+            ((zerop covered-mines)
+             (format nil "You won!!"))
+                (T
+                 (format nil "Uncover ~A mines more" covered-mines))))))
+
+(defun process-pressed-1 ()
+  (let ((box-under-mouse
+          (first
+           (remove-if-not
+            (lambda (c) (mouse-over-p c))
+            (children (grid *model*))))))
+    (when box-under-mouse
+      (cond ((null (state box-under-mouse))
+             (setf (state box-under-mouse) :empty))
+            ((eq :covered-mine (state box-under-mouse))
+             (setf (state box-under-mouse) :exploded-mine)))))
+  (mine-stats))
+
+(defun process-pressed-3 ()
+  (let ((box-under-mouse
+          (first
+           (remove-if-not
+            (lambda (c) (mouse-over-p c))
+            (children (grid *model*))))))
+    (when box-under-mouse
+      (cond ((null (state box-under-mouse))
+             (setf (state box-under-mouse) :flagged-empty))
+            ((eq :covered-mine (state box-under-mouse))
+             (setf (state box-under-mouse) :flagged-mine))
+
+            ((eq :flagged-empty (state box-under-mouse))
+             (setf (state box-under-mouse) nil))
+            ((eq :flagged-mine (state box-under-mouse))
+             (setf (state box-under-mouse) :covered-mine)))))
+  (mine-stats))
---
+++ gui-drawing.lisp
@@ -0,0 +1,122 @@
+(in-package #:clos-sweeper)
+
+;;; !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+(cffi:defcstruct gdk-rgba
+  (red   :float)
+  (green :float)
+  (blue  :float)
+  (alpha :float))
+
+(defun color-to-rgba (color)
+  (cffi:with-foreign-object (rgba '(:struct gdk-rgba))
+    (let ((pointer (make-instance 'gir::struct-instance
+                                  :class (gir:nget gdk::*ns* "RGBA")
+                                  :this rgba)))
+      (let ((valid-color (gdk:rgba-parse pointer color)))
+        (cffi:with-foreign-slots ((red green blue alpha) rgba (:struct gdk-rgba))
+          (list valid-color red green blue alpha))))))
+
+(defun set-rgba (color)
+  (let ((parsed-color (color-to-rgba color)))
+    (if (first parsed-color)
+        (apply 'cairo:set-source-rgba (rest parsed-color))
+        (error "~S is not a valid color" color))))
+;;; !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
+;;; drawing callback ===========================================================
+(cffi:defcallback %draw-func :void ((area :pointer)
+                                    (cr :pointer)
+                                    (width :int)
+                                    (height :int)
+                                    (data :pointer))
+  (declare (ignore area data))
+
+  (setf cairo:*context* (make-instance 'cairo:context
+                                       :pointer cr
+                                       :width width
+                                       :height height
+                                       :pixel-based-p nil))
+  ;; ###########################################################################
+
+  ;; call actual drawing
+  (draw-objects (coerce (the (signed-byte 32) width)  'single-float)
+                (coerce (the (signed-byte 32) height) 'single-float)))
+
+(defun simulate-draw-func (w h)
+  (let* ((surface (cairo:create-image-surface :argb32
+                                              w
+                                              h)))
+    (let* ((ctx (cairo:create-context surface)))
+      (setf  cairo:*context* ctx)
+      ;; #######################################################################
+
+      ;; call actual drawing
+      (draw-objects w
+                    h))
+
+    ;; put drawn surface to a file
+    (cairo:surface-write-to-png surface
+                                (format nil
+                                        "/tmp/cairo-simulate-drawing-~A.png"
+                                        (get-internal-run-time)))))
+
+;;; ============================ view ==========================================
+(defun draw-objects (w h)               ; view
+  ;; (format t ">>>>>>>> in draw-objects ~S " cairo:*context*)
+  (let ((cnt (format nil "~S" (selection *model*)))
+        (tw 0)
+        (th 0)
+        (tpx 0)
+        (tpy 0))
+    (cairo:set-source-rgb 1 1 1)
+    (cairo:paint)
+
+    (render (grid *model*))
+
+
+    (cairo:select-font-face "Ubuntu Mono" :normal :bold)
+    (cairo:set-font-size 20)
+    (multiple-value-bind  (xb yb width height)
+        (cairo:text-extents cnt)
+      (declare (ignore xb yb))
+      (setf
+       tw width
+       th height))
+    (setf tpx 6
+          tpy 15)
+    (cairo:set-source-rgb 0 0 0)
+    (cairo:move-to tpx tpy)
+    (cairo:show-text (format nil "~A" cnt))
+
+
+
+
+    (let ((bottom-text (format nil "mouse: ~Sx~S, canvas: ~Ax~A"
+                               (mouse-x *model*) (mouse-y *model*) w h) ))
+      (multiple-value-bind (xp yp width height)
+          (cairo:text-extents bottom-text)
+        (declare (ignore xp yp))
+        (cairo:move-to (- w (* 1.01 width))
+                       (- h (* 0.3 height)))
+        (cairo:show-text bottom-text)))
+
+
+    ;; circle at mouse coordinates
+    (when (and (null *canvas*)
+               (mouse-x *model*)
+               (mouse-y *model*))
+      (cairo:set-source-rgba 1 1 0.2 0.4)
+      (cairo:set-line-width 6.0)
+      (cairo:arc (mouse-x *model*)
+                 (mouse-y *model*)
+                 10.0 0 (* 2.0 pi))
+      (cairo:fill-path)
+      (cairo:arc (mouse-x *model*)
+                 (mouse-y *model*)
+                 10.0 0 (* 2.0 pi))
+      (set-rgba "green")
+      (cairo:set-line-width 0.5)
+      (cairo:stroke))
+
+
+    ))
