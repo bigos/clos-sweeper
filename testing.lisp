f0e0ecfb0c59d3825292adf4fde56f4c554294dc
Jacek Podkanski <ruby.object@googlemail.com>
2023-12-05 23:48:16 +0000

    testing instructions

--- testing.lisp
+++ testing.lisp
@@ -6,8 +6,8 @@
   (format t "running testing~%")
 
   (push #p"/home/jacek/Programming/Lisp/clos-sweeper/" asdf:*central-registry*)
-  (ql:quickload :clos-sweeper/tests)
   (asdf:test-system :clos-sweeper/tests)
+
   (sb-ext:exit))
 
 (testing)
