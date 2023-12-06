a633a1dc4c0230439754f637914e52b2e3482952
Jacek Podkanski <Jacek.Podkanski@conceptlifesciences.com>
2023-12-06 17:02:55 +0000

    testing

--- tests/clos-sweeper-tests.lisp
+++ tests/clos-sweeper-tests.lisp
@@ -13,7 +13,26 @@
       (is (= 4 (* 2 2))))
 
 (test grid-building
-      "test building grid without the arguments"
-  (let ((b (build-grid)))
+  "test building grid without the arguments"
+  ;; we need to init model for the global *model* variable
+  (clos-sweeper::init-model)
+  (let ((b (clos-sweeper::build-grid)))
     (is (not (null b)))
-    (is (type-of b) 'grid)))
+    (is (type-of b) 'grid)
+    (is (= (* 8 8) (length (clos-sweeper::children b)))))
+  (let ((b (clos-sweeper::build-grid 8)))
+    (is (not (null b)))
+    (is (type-of b) 'grid)
+    (is (= (* 8 8) (length (clos-sweeper::children b)))))
+
+  (let ((b (clos-sweeper::build-grid 16)))
+    (is (not (null b)))
+    (is (type-of b) 'grid)
+    (is (= (* 16 16) (length (clos-sweeper::children b)))))
+
+  (let ((b (clos-sweeper::build-grid 32)))
+    (is (not (null b)))
+    (is (type-of b) 'grid)
+    (is (= (* 32 32) (length (clos-sweeper::children b)))))
+  ;; find error detection
+  (signals (clos-sweeper::build-grid 15)))
