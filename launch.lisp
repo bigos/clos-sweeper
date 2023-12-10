2a3d0115b941a2f2629af373b6544a4c0c41c05f
Jacek Podkanski <ruby.object@googlemail.com>
2023-12-10 00:23:02 +0000

    launcher works in power shell

---
+++ launch.lisp
@@ -0,0 +1,27 @@
+;;; run it in the terminal with:
+;;; sbcl --load  ~/Programming/Lisp/clos-sweeper/launch.lisp
+
+
+(defun launch ()
+  (format t "running launcher on ~A~%" (uiop/os:detect-os))
+
+  (if (eq (uiop/os:detect-os) :os-windows)
+      (progn
+        (format T "registering code with Windows quicklisp~%")
+        (push #p"c:/Users/jacek/Programming/Lisp/clos-sweeper/" asdf:*central-registry*))
+      (progn
+        (format T "registering code with Linux quicklisp~%")
+        (push #p"~/Programming/Lisp/clos-sweeper/" asdf:*central-registry*)))
+
+  (format t "loading clos sweeper~%")
+  (ql:quickload :clos-sweeper)
+  (sleep 1)
+  (format t "RUNNING clos sweeper~%")
+
+  (uiop:symbol-call :clos-sweeper :main)
+
+  ;; (sb-ext:exit)
+
+  )
+
+(launch)
