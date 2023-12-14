8e10e9c87566f4673f248db3ffa1ab3ca338c6fe
Jacek Podkanski <Jacek.Podkanski@conceptlifesciences.com>
2023-12-14 13:42:49 +0000

    now I can generate linux executable using a script

--- build-linux-executable.lisp
+++ build-linux-executable.lisp
@@ -2,9 +2,4 @@
 
 (push #p"~/Programming/Lisp/clos-sweeper/" asdf:*central-registry*)
 
-(ql:quickload :clos-sweeper)
-(use-package :clos-sweeper)
-
-(sb-ext:save-lisp-and-die "win-clos-sweeper"
-                          :toplevel #'main
-                          :executable t)
+(asdf:make :clos-sweeper)
