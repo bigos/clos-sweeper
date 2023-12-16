2d1bc728086fbca3f346c1a95efbf8ebf7670412
Jacek Podkanski <ruby.object@googlemail.com>
2023-12-16 13:05:43 +0000

    copying everything works

--- build_distribution.sh
+++ build_distribution.sh
@@ -13,8 +13,15 @@ mkdir -p "./distribution/bin"
 
 echo "copying lisp executable"
 cp -v ./src/clos-sweeper.exe ./distribution/bin/
+
 echo "copying ALL DLLs"
-cp -v /mingw64/bin/*.dll ./distribution/bin/
+cp -v /mingw64/bin/* ./distribution/bin
+
+# cp -v /mingw64/bin/libgtk-4-1.dll ./distribution/bin
+# cp -v /mingw64/bin/libcairo-2.dll ./distribution/bin
+# cp -v /mingw64/bin/libgirepository-1.0-1.dll ./distribution/bin
+# cp -v /mingw64/bin/libgobject-2.0-0.dll ./distribution/bin
+# cp -v /mingw64/bin/libwinpthread-1.dll ./distribution/bin
 
 echo "preparing share folders"
 mkdir -p "./distribution/share/glib-2.0"
@@ -23,5 +30,15 @@ mkdir -p "./distribution/share/icons"
 
 echo "copying share folders"
 cp -rv /mingw64/share/glib-2.0/schemas ./distribution/share/glib-2.0/
-cp -rv /mingw64/share/icons/Adwaita    ./distribution/share/icons/ 
+cp -rv /mingw64/share/icons/Adwaita    ./distribution/share/icons/
 cp -rv /mingw64/share/icons/hicolor    ./distribution/share/icons/
+
+echo "copying all the rest"
+cp -rv /mingw64/etc ./distribution/
+cp -rv /mingw64/include ./distribution/
+cp -rv /mingw64/lib ./distribution/
+cp -rv /mingw64/libexec ./distribution/
+cp -rv /mingw64/share ./distribution/
+cp -rv /mingw64/var ./distribution/
+
+echo "==============done==================="
