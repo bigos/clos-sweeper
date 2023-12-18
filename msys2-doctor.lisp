bee7b7f5d4055c7d4bb6e47716f4b71821453fde
Jacek Podkanski <ruby.object@googlemail.com>
2023-12-18 00:20:15 +0000

    msys2 doctor

--- msys2-doctor.lisp
+++ msys2-doctor.lisp
@@ -12,18 +12,21 @@
   (if (eq :os-windows (uiop/os:detect-os))
       (progn
         (format t "OS check OK~%")
-        (if (uiop/filesystem:directory-exists-p #p "c:/msys64/mingw64/bin")
+        (if (search "msys64/mingw64/bin" (uiop/os:getenv "PATH"))
             (progn
-              (format t "MSYS2 bin folder check OK~%~%")
+              (format t "MSYS2 bin is found in environmet variable PATH~%")
+              (if (uiop/filesystem:directory-exists-p #p "c:/msys64/mingw64/bin")
+                  (progn
+                    (format t "MSYS2 bin folder check OK~%~%")
 
-              (examine-library #p "c:/msys64/mingw64/bin/libgtk-4-1.dll" "gtk")            
-              (examine-library "c:/msys64/mingw64/bin/libcairo-2.dll" "cairo")
-              (examine-library "c:/msys64/mingw64/bin/libglib-2.0-0.dll" "glib")
-              (examine-library "c:/msys64/mingw64/bin/libgio-2.0-0.dll" "gio")
-              (examine-library "c:/msys64/mingw64/bin/libgirepository-1.0-1.dll" "girrepository")
-              (examine-library "c:/msys64/mingw64/bin/libgobject-2.0-0.dll" "gobject")
-              (examine-library "c:/msys64/mingw64/bin/libwinpthread-1.dll" "winpthread"))
-            (error "directory not found ~A" "C:\\msys64\\mingw64\\bin")))
+                    (examine-library #p "c:/msys64/mingw64/bin/libgtk-4-1.dll" "gtk")            
+                    (examine-library "c:/msys64/mingw64/bin/libcairo-2.dll" "cairo")
+                    (examine-library "c:/msys64/mingw64/bin/libglib-2.0-0.dll" "glib")
+                    (examine-library "c:/msys64/mingw64/bin/libgio-2.0-0.dll" "gio")
+                    (examine-library "c:/msys64/mingw64/bin/libgirepository-1.0-1.dll" "girrepository")
+                    (examine-library "c:/msys64/mingw64/bin/libgobject-2.0-0.dll" "gobject"))
+                  (error "directory not found ~A" "C:\\msys64\\mingw64\\bin")))
+            (error "MSYS2 bin path not found in the PATH environmental variable")))
       (error "Expected Windows but found ~A" (uiop/os:detect-os)))
   ;; the end
   )
