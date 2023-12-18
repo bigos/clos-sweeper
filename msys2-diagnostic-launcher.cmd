914facdbe8c4ab207726a4bd7e880898d01e15fd
Jacek Podkanski <ruby.object@googlemail.com>
2023-12-18 04:14:54 +0000

    checking for expected folders

--- msys2-diagnostic-launcher.cmd
+++ msys2-diagnostic-launcher.cmd
@@ -1,5 +1,22 @@
 rem msys2 launcher
 
+rem https://devblogs.microsoft.com/powershell-community/determine-if-a-folder-exists/
+
+set BINDIR1=C:\msys64\mingw64\bin
+if exist %BINDIR1% (
+echo "Expected folder 1 exists"
+) else (
+echo "expected folder 1 does not exist"
+)
+
+
+set BINDIR2=C:\msys64\usr\bin
+if exist %BINDIR2% (
+echo "Expected folder 2 exists"
+) else (
+echo "expected folder 2 does not exist"
+)
+
 set PATH=c:/msys64/mingw64/bin;c:/msys64/usr/bin;%PATH%
 
 src\clos-sweeper.exe
